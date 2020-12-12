abd9591c52d41b6ae40521fd305ffc91e4fa7769
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2020-12-12 10:20:41 -0600

    reconnected to repo

---
+++ README.md
@@ -0,0 +1,80 @@
+# App Title: Barista Expertista
+
+https://github.com/skeebs10/README.git
+
+## Concept:
+
+This game is designed to test your knowledge on various types of coffee drinks served in coffeeshops worldwide! You (Barista 1) will be up against the coffeehouse (Barista 2) and tasked with selecting the correct coffee type for each given recipe. 
+For every incorrect match, you lose a point. If you lose all your points you have to restart the game and the coffeehouse (Baritsa 2 wns!). Keep in mind that you only have 3 points to complete this 10 drink menu and be crowned the ultimate "Barista Expertisa"! Feel free to restart the challenge if you make it to the end. Good luck!
+
+
+## Technologies Used:
+
+* HTML
+* CSS
+* JavaScript 
+* JQuery 
+* External JavaScript Library
+
+##### Credits:
+
+
+## Approach:
+I will approach this with wireframes and arrays. Created two players (Barista 1 & 2) and add two answer options to choose from each round. If you lose all beans you go back to start. Want ot make this game all coffee theme. 
+#### Overview
+
+#### Wireframe
+
+<img src="/Wireframe drawn.png">
+
+This is handrawn I will upload a photo. 
+
+Color Pallet:
+
+I'll be looking through browns in coolors.com.
+
+#### User stories
+
+As an user I expect to navigate through the game with a start, next, and restart button. The bean count will change as the game goes on. I expect this game to be extremely difficult as I chose the most random coffee types. 
+
+#### Development Plan 
+
+* Development Plan  ONE- I will brainstorm, do research, and wireframe the best way to make it interactive. I am very inspired with a project I saw in class from last semester and super excited to try and complete a coffee game being a coffee enthusiast! (Day 1-2)
+* Development Plan  Two - The plan for HTML is create a game like console. I will plan to create a container with the score on top, picture displayed in the middle and a bottom of consol controls or buttons. More wireframes (Day 2-3)
+* Development Plan  Three-HTML/CSS I will play around with css and try to add cool features. Definitely feeling a brown theme since this is a coffee game. I found a webiste called coolors where I will look for trendy patterns for flow. (Day 2)
+* Development Plan  Four - For JQUERY, I will go ahead and create and array of objects where I will store each question similar to a powerpoint. I will not loop but use the next button to move along with the coutner. Then debug and fix it up (Day 3-6).
+
+Time Display
+
+
+<img src="/Users/nataliaskiba/Desktop/code/README/TIme Display Grid.heic>
+
+
+
+
+
+
+
+
+
+#### MVP
+
+* MVP ONE- Getting all the images in the correct position.
+* MVP Two- Restarting the game when 3 beans gone.  
+* MVP Three - Adding more than one player to the game.  In my head it's more of a single player challenge. I guess that's the challenge! Compared it to blackjack when you play the house or in this case coffeehouse
+* MVP Four
+
+#### Stretch goals
+
+* Stretch goals ONE- Go back and figure out how to loop 
+* Stretch goals Two- Apply Carrousal lesson and loop through 
+* Stretch goals Three- Make it very interactive. 
+* Stretch goals Four- Never enough visuals!
+
+## Challenges:
+
+I really wanted to incorporate the corgi carousel jquery exercise we did in class for my project. Unfortunately I could not figure out how to use it for my game given. Found it easier to create an array of objects. This project was tough. I changed my mind multiple time and lost a few days of work just by starting over and going to the drawing board. Also not sure if I was committing correctly going between the Github enterprise and my git. I revisited class lectures frequently which was time consuming.
+
+
+
+### App Demo: 
---
+++ app.js
@@ -0,0 +1,264 @@
+$(() => {
+    // Questions in Array
+    const questions = [
+        {
+            image: "images/Espresso.png",
+            answers: [
+                { text: 'Espresso', correct: true, points: 0},
+                { text2: 'Cortado', correct2: false, points2: -1 },
+            ]
+        },
+        {
+            image: "images/Americano.png",
+            answers: [
+                { text: 'Affogato', correct: false, points: -1},
+                { text2: 'Americano', correct2: true, points2: 0 },
+            ]
+        },
+        {
+            image:"images/Cafe au Lait.png",
+            answers: [
+                { text: 'Affogato', correct: false, points: -1},
+                { text2: 'Cafe au Lait', correct2: true, points2: 0 },
+            ]
+        },
+        {
+            image: "images/Caffe Breve.png" ,
+            answers: [
+                { text: 'Breve', correct: true, points: 0 },
+                { text2: 'Cold Brew', correct2: false, points2: -1},
+            ]
+        },
+        {
+            image: "images/Caffe Latte.png" ,
+            answers: [
+                { text: 'Caffe Latte', correct: true, points: 0 },
+                { text2: 'Irish Coffee', correct2: false, points2: -1 },
+            ]
+        },
+        {
+            image: "images/Caffe Mocha.png",
+            answers: [
+                { text: 'Lungo', correct: false, points: -1 },
+                { text2: 'Caffe Mocha', correct2: true, points2: 0},
+            ]
+        },
+        {
+            image: "images/Espresso con Panna.png",
+            answers: [
+                { text: 'Con Panna', correct: true, points: 0 },
+                { text2: 'Flat White', correct2: false, points2: -1 }
+            ]
+        },
+        {
+            image: "images/Espresso Macchiato.png" ,
+            answers: [
+                { text: "Machiato", correct: true, points: 0},
+                { text2: 'Mocha', correct2: false, points2: -1 },
+            ]
+        },
+        {
+            image: "images/Flat White.png",
+            answers: [
+                { text: 'Doppio', correct: false, points: -1},
+                { text2: 'Flat White', correct2: true, points2: 0 },
+            ]
+        },
+        {
+            image: "images/Red Eye.png" ,
+            answers: [
+                { text: 'Red Eye', correct: true, points: 0 },
+                { text2: 'Ristretto', correct2: false, points2: -1 },
+            ]
+        }
+    ]
+
+    // Cached Dom Nodes
+    const $startButton = $('#start-btn');
+    const $nextButton = $('#next-btn');
+    const $questionImage = $('#question-image');
+    const $answerButtons= $('#answer-buttons');
+    const $progressBarFull = $('#progressBarFull'); 
+    const $modalText =$('modal-box');
+    const $openModal =$('rules-btn');
+    const $closeModal =$('#close');
+    const $points= $('#bean');
+    const $btn1= $('#btn1');
+    const $btn2= $('#btn2');
+    const $imageContainer= $('#imageContainer');
+    const $progress= $('#progress');
+    const $rules_btn= $('#rules-btn');
+    const $title= $('#title');
+
+    // Variables
+    const highestIndex = questions.length;
+    let currentQuestionIndex = 0;
+    let isCorrect = false;
+
+    // Player Class
+    class Player {
+        constructor(name, beans) {
+            this.name = name;
+            this.beans = beans
+        }
+    }
+
+    // The Point is to have to have more Beans than the Barista Expertista to be Queen Winner!
+    const barista1 = new Player('Barista 1', 3); // You, the user, starts with 3 beans.
+    const barista2= new Player('Barista 2', 0); // Barista 2 is the Barista Expertista. You want to come on top!
+
+    //Open Modal Function
+    const brewles =() => {
+        $modal.css('display', 'flex');
+
+    }
+    //Close Modal Function
+    const closeModal =()=> {
+        $modal.css('display', 'none');
+    }
+    // Start Game
+    const startGame = () => {
+        $startButton.addClass('hide');
+
+        $btn1.remove('hide');
+        $btn2.remove('hide');
+        $imageContainer.remove('hide');
+        $progress.remove('hide');
+
+        $btn1.addClass('show');
+        $btn2.addClass('show');
+        $imageContainer.addClass('show');
+        $progress.addClass('show');
+
+
+        setNextQuestion();
+    }
+
+    function showQuestion(q) {
+        console.log("next")
+        console.log(q);
+        $btn1.html(questions[q]["answers"][0]['text']);
+        $btn2.html(questions[q]["answers"][1]['text2']);
+        updateProgress();
+
+
+        $progress.addClass('hide');
+        $rules_btn.addClass('hide');
+        $title.addClass('hide');
+
+        $progress.remove('show');
+        $rules_btn.remove('show');
+        $title.remove('show');
+
+    };
+///////Shows you where you're at in the game Update Progress//////////////
+    const updateProgress = () =>{
+        $progress.html(currentQuestionIndex + "/" + highestIndex);
+    }
+////////Restart Function///////////
+    const restart = () =>{
+        currentQuestionIndex = 0;
+        barista1.beans = 3;
+        setNextQuestion();
+        $points.html("<h3>" + barista1.beans + "</h3>")
+
+        $progress.remove('hide');
+        $rules_btn.remove('hide');
+        $title.remove('hide');
+
+        $progress.addClass('show');
+        $rules_btn.addClass('show');
+        $title.addClass('show');
+
+        $btn1.remove('show');
+        $btn2.remove('show');
+
+        $btn1.addClass('hide');
+        $btn2.addClass('hide');
+
+        startGame();
+    }
+
+    const setNextQuestion = () => {
+        showQuestion(currentQuestionIndex);
+    }
+
+    const setChoice1 = () => {
+        if(currentQuestionIndex >= highestIndex) {
+            alert("YOU ARE THE QUEEN BEAN! HOUSE BARISTA LOST");
+            restart();
+        }else{
+            isCorrect = questions[currentQuestionIndex]["answers"][0]['correct'];
+            $nextButton.click();
+        }
+    }
+    const setChoice2 = () => {
+        if(currentQuestionIndex >= highestIndex) {
+            alert("YOU WON!");
+            restart();
+        }else{
+            isCorrect = questions[currentQuestionIndex]["answers"][1]['correct2'];
+            $nextButton.click();
+        }
+    }
+
+
+    const nextImg = () => {
+        currentQuestionIndex++;
+
+        if(currentQuestionIndex >= highestIndex && barista1.beans > 0) {
+            alert("YOU ARE THE QUEEN BEAN!");
+            restart();
+        }else if(currentQuestionIndex < highestIndex){
+            if(barista1.beans <= 0){
+                alert("YOU LOST! HOUSE BARISTA WON");
+                restart();
+            }else{
+                let tempImg = questions[currentQuestionIndex]["image"];
+                $imageContainer.attr("src", tempImg);
+                showQuestion(currentQuestionIndex);
+                if(isCorrect){
+
+                }else{
+                    barista1.beans = barista1.beans - 1;
+                    $points.html("<h3>" + barista1.beans + "</h3>")
+                    if(barista1.beans <= 0){
+                        alert("You lost.");
+                        restart();
+                    }
+                }
+
+            }
+        }
+
+
+    }
+
+        // EVENT LISTENERS
+        $startButton.on('click', startGame)
+        $nextButton.on('click', nextImg);
+        $btn1.on('click', setChoice1);
+        $btn2.on('click', setChoice2);
+
+
+
+        //OPEN & CLOSE MODAL
+        $openModal.on('click', brewles);
+        $closeModal.on('click', closeModal);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
